51cb6279fa80c962359909b12896bc9e5e9b9165
PS <piotr.stepien@skillandchill.com>
2025-01-28 18:21:13 +0100

    fix

--- middleware.ts
+++ middleware.ts
@@ -6,11 +6,9 @@ const defaultLocale = "pl-PL";
 
 export function middleware(request: NextRequest) {
   const lang = request.nextUrl.pathname.split("/")[1];
-  console.log("lang" + lang);
   if (!validLocales.includes(lang)) {
     const newUrl = new URL(request.url);
     newUrl.pathname = `/${defaultLocale}`;
-    console.log("Redirect " + lang + " to " + defaultLocale);
     return NextResponse.redirect(newUrl);
   }
 
