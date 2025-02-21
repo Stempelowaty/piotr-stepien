7a043cae5f55b5d5426845674ebee53c3b1e96c1
PS <pi.ad.stepien@gmail.com>
2025-02-21 15:39:00 +0100

    minor spelling mistake

--- app/_components/segments/aboutMe.tsx
+++ app/_components/segments/aboutMe.tsx
@@ -33,7 +33,7 @@ const getDictionary = (locale: string) => {
       about: "O mnie",
       chapterTitle: "Kim jest ten czarujący typek?",
       chapterOne:
-        "Nie wiem czy czarujący, ale na zdjęciu jestem ja. Zawsze fascynowały mnie gry. Po modyfikowaniu gry Skyrim dwukrotnie dłużej niż w nią grałem i rozłożeniu pierwszych gier z serii Doom na czynniki pierwsze, postanowiłem zostać programistą. Podczas studiów inżynierskich, rozwijałem wiele projektów gier, korzystając z różnych frameworków.",
+        "Zawsze fascynowały mnie gry. Po modyfikowaniu gry Skyrim dwukrotnie dłużej niż w nią grałem i rozłożeniu pierwszych gier z serii Doom na czynniki pierwsze, postanowiłem zostać programistą. Podczas studiów inżynierskich, rozwijałem wiele projektów gier, korzystając z różnych frameworków.",
       chapterTwo:
         "Moja pasja poszerzyła się o nowe technologie i po pewnym czasie zdobyłem moją pierwszą pracę jako programista full-stack. Tam nauczyłem się korzystać z Reacta i innych trudnych słów, które brzmią jak nazwy dla Pokemonów.",
     },
@@ -41,7 +41,7 @@ const getDictionary = (locale: string) => {
       about: "About me",
       chapterTitle: "Who is this charming fellow?",
       chapterOne:
-        "I don't know if charming is the right word, but that's me up there. I have always been fascinated by video games. After modding Skyrim for twice as long as I've played it and dissecting the first Doom games, I decided to become a software developer. While studying for my engineering degree, I developed a variety of game projects using various frameworks.",
+        "I have always been fascinated by video games. After modding Skyrim for twice as long as I've played it and dissecting the first Doom games, I decided to become a software developer. While studying for my engineering degree, I developed a variety of game projects using various frameworks.",
       chapterTwo:
         "My passion for programming expanded into other technologies, and after a while, I landed my first job as a full-stack developer. There, I learned about React and other quirky terms that sound like names for Pokémon.",
     },
